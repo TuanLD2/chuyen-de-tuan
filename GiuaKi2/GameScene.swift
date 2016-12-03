de8b92f260d6ea43d365db8ea2d056bd046c1ef1
TuanLD2 <tuanbaoboitinhyeu@gmail.com>
2016-12-03 08:49:53 -0800

    update

---
+++ GiuaKi2/GameScene.swift
@@ -0,0 +1,29 @@
+//
+//  GameScene.swift
+//  GiuaKi2
+//
+//  Created by Huỳnh Thanh Tuấn on 12/2/16.
+//  Copyright (c) 2016 Huỳnh Thanh Tuấn. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMoveToView(view: SKView) {
+
+        /*let x = 1000
+        let y = 500
+        let backgroundTexture = SKTexture(imageNamed: "theothao.jpg")
+        let backgroundImage = SKSpriteNode(texture: backgroundTexture, size: CGSize(width: x, height: y ))
+        backgroundImage.position = CGPoint(x: x/2,y: y/2)
+        addChild(backgroundImage)*/
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+            }
+   
+    override func update(currentTime: CFTimeInterval) {
+        
+    }
+}
